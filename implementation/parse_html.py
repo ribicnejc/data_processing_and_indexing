62834071862ba9892fe490716f610b3a037fc3dc
ExPie <nomaail@nomail.net>
2019-05-27 20:51:53 +0200

    Added neighbourhood to postings

--- implementation/parse_html.py
+++ implementation/parse_html.py
@@ -8,9 +8,7 @@ html_cleaner.ignore_images = True
 html_cleaner.ignore_links = True
 html_cleaner.ignore_tables = True
 
-
 tknzr = TweetTokenizer()
-
 
 # build stopword set
 stopwords = set()
@@ -18,6 +16,24 @@ with open("data/slovenian-stopwords.txt", "r", encoding="utf-8") as stopword_fil
     stopword_file_contents = stopword_file.readlines()
 for stopword in stopword_file_contents:
     stopwords.add(stopword.strip())
+
+
+def get_neighbours(tokens, index):
+    start = index - 3
+    end = index + 3
+    if start < 0:
+        start = 0
+    if end > len(tokens):
+        end = len(tokens)
+
+    neighbours = ""
+
+    i = start
+    while i < end:
+        neighbours += tokens[i] + " "
+        i += 1
+
+    return neighbours.strip()
 
 
 def print_html(path):
@@ -34,22 +50,20 @@ def print_html(path):
     # lower all tokens
     low_tokens = [word.lower() for word in tokens]
 
-
     # remove nonwords with re and stopword using a list
     posting = {}
 
     for index, word in enumerate(low_tokens):
         if re.match("[a-zA-Z0-9][a-zA-Z0-9-\.]+", word) and word not in stopwords:
-            #print(str(index) + ": " + word)
+            # print(str(index) + ": " + word)
             posting.setdefault(word, [])
-            posting[word].append(index)
+            posting[word].append((index, get_neighbours(low_tokens, index)))
 
     for key, val in posting.items():
         print(key, end=": ")
         for idx in val:
             print(str(idx), end=", ")
         print()
-
 
 
 print_html("data/evem.gov.si/evem.gov.si.1.html")
