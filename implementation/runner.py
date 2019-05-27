6a6939df32b23a09b3ec3405147e7f4e157f343d
ExPie <nomaail@nomail.net>
2019-05-27 21:47:00 +0200

    DB fix

--- implementation/runner.py
+++ implementation/runner.py
@@ -1,12 +1,17 @@
 from os import listdir
 from os.path import isdir, isfile, join
 from implementation.parse_html import print_html
+from implementation.index_builder import clear_database
+
+
+clear_database()
 
 
 def read_files(path):
     for f in listdir(path):
         if isfile(join(path, f)):
             if f.endswith(".html"):
+                print("Parsing file: " + f)
                 print_html(join(path, f))
         else:
             read_files(join(path, f))
