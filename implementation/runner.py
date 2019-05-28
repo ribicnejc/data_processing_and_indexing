d2d9e790e3a3e284b2cc6bc567e4ec54d5fbc5d7
ExPie <nomaail@nomail.net>
2019-05-28 18:41:42 +0200

    Cleaned structure

--- implementation/runner.py
+++ implementation/runner.py
@@ -1,16 +1,20 @@
 from os import listdir
-from os.path import isdir, isfile, join
+from os.path import isfile, join
 from implementation.parse_html import Parser
-from implementation.index_builder import clear_database
 from implementation.db_manager import dbManager
 
 parser = Parser(dbManager)
+
+doc_parsed = 0
+
 
 def read_files(path):
     for f in listdir(path):
         if isfile(join(path, f)):
             if f.endswith(".html"):
                 parser.print_html(join(path, f))
+                doc_parsed += 1
+                print("Documents parsed: " + str(doc_parsed))
         else:
             read_files(join(path, f))
 
