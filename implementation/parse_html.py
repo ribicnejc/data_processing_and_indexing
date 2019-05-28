9467c3d9a6fd4f3cd5806684fd43dda7127181f3
ExPie <nomaail@nomail.net>
2019-05-28 17:29:55 +0200

    Refactor into a class

--- implementation/parse_html.py
+++ implementation/parse_html.py
@@ -4,73 +4,79 @@ import re
 from _functools import reduce
 from implementation.index_builder import insert_to_index
 
-html_cleaner = html2text.HTML2Text()
-html_cleaner.ignore_emphasis = True
-html_cleaner.ignore_images = True
-html_cleaner.ignore_links = True
-html_cleaner.ignore_tables = True
 
-tknzr = TweetTokenizer()
+class Parser:
+    def __init__(self):
+        self.html_cleaner = html2text.HTML2Text()
+        self.html_cleaner.ignore_emphasis = True
+        self.html_cleaner.ignore_images = True
+        self.html_cleaner.ignore_links = True
+        self.html_cleaner.ignore_tables = True
 
-# build stopword set
-stopwords = set()
-with open("data/slovenian-stopwords.txt", "r", encoding="utf-8") as stopword_file:
-    stopword_file_contents = stopword_file.readlines()
-for stopword in stopword_file_contents:
-    stopwords.add(stopword.strip())
+        self.tknzr = TweetTokenizer()
+        self.stopwords = set()
 
-
-def get_neighbours(tokens, index):
-    start = index - 3
-    end = index + 3
-    if start < 0:
-        start = 0
-    if end > len(tokens):
-        end = len(tokens)
-
-    neighbours = ""
-
-    i = start
-    while i < end:
-        neighbours += tokens[i] + " "
-        i += 1
-
-    return neighbours.strip()
+        self.tokens = []
 
 
-def print_html(path):
-    # get file content
-    with open(path, "r", encoding="utf-8") as content_file:
-        content = content_file.read()
+    def build_stopwords(self):
+        with open("data/slovenian-stopwords.txt", "r", encoding="utf-8") as stopword_file:
+            stopword_file_contents = stopword_file.readlines()
+        for stopword in stopword_file_contents:
+            self.stopwords.add(stopword.strip())
 
-    # get text from html
-    clean_text = html_cleaner.handle(content)
 
-    # tokenize the text
-    tokens = tknzr.tokenize(clean_text)
+    def get_neighbours(self, index):
+        start = index - 3
+        end = index + 3
+        if start < 0:
+            start = 0
+        if end > len(self.tokens):
+            end = len(self.tokens)
 
-    # lower all tokens
-    low_tokens = [word.lower() for word in tokens]
+        neighbours = ""
 
-    # remove nonwords with re and stopword using a list
-    # posting type:    word: [(index, neighbour), ...]
-    # build a dict
-    posting = {}
-    for index, word in enumerate(low_tokens):
-        if re.match("[a-zA-Z0-9][a-zA-Z0-9-\.]+", word) and word not in stopwords:
-            # print(str(index) + ": " + word)
-            posting.setdefault(word, [])
-            posting[word].append((index, get_neighbours(low_tokens, index)))
+        i = start
+        while i < end:
+            neighbours += self.tokens[i] + " "
+            i += 1
 
-    # insert into db
-    for key, val in posting.items():
-        print(key)
-        indices = str(list(map(lambda x: x[0], val))).strip("[]").replace(" ", "")
-        print(indices)
-        neighbourhood = "\n".join(list(map(lambda x: x[1].strip(), val)))
-        print(neighbourhood + "\n")
+        return neighbours.strip()
 
-    print(len(posting))
+
+    def print_html(self, path):
+        # get file content
+        with open(path, "r", encoding="utf-8") as content_file:
+            content = content_file.read()
+
+        # get text from html
+        clean_text = self.html_cleaner.handle(content)
+
+        # tokenize the text
+        self.tokens = self.tknzr.tokenize(clean_text)
+
+        # lower all tokens
+        low_tokens = [word.lower() for word in self.tokens]
+
+        # remove nonwords with re and stopword using a list
+        # posting type:    word: [(index, neighbour), ...]
+        # build a dict
+        posting = {}
+        for index, word in enumerate(low_tokens):
+            if re.match("[a-zA-Z0-9][a-zA-Z0-9-\.]+", word) and word not in self.stopwords:
+                # print(str(index) + ": " + word)
+                posting.setdefault(word, [])
+                posting[word].append((index, self.get_neighbours(index)))
+
+        # insert into db
+        for key, val in posting.items():
+            print(key)
+            indices = str(list(map(lambda x: x[0], val))).strip("[]").replace(" ", "")
+            print(indices)
+            neighbourhood = "\n".join(list(map(lambda x: x[1].strip(), val)))
+            print(neighbourhood + "\n")
+
+        print(len(posting))
 
 
 
@@ -85,5 +91,8 @@ def print_html(path):
             # print("LEN: " + str(len(val)))
             # print(str(reduce(lambda a,b: a+str(b)+",", map(lambda x: x[0], val), ""))[:-1])
 
-print_html("data/evem.gov.si/evem.gov.si.1.html")
+parser = Parser()
+parser.print_html("data/evem.gov.si/evem.gov.si.1.html")
+
+
 
